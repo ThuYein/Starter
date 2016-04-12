e0797e81832c381b845adce7e58aa2c715582b54
ThuYein <thuyeintun@my.jcu.edu.au>
2016-04-12 13:09:54 +0800

    Changed to function

--- converter.py
+++ converter.py
@@ -1,9 +1,13 @@
+def fahrenheit(celsius):
+    return celsius * 9 / 5 + 32
+
+def kelvin(fahrenheit):
+    return (fahrenheit + 459.67) * 5 / 9
+
 print('Temperature Conversion Program')
 
 celsiusVlaue = float(input('Enter temperature in Celsius: '))
-fahrenheitValue = celsiusVlaue * 9 / 5 + 32
-kelvinValue = (fahrenheitValue + 459.67) * 5 / 9
 
 print('Celsius Value: ', celsiusVlaue)
-print('Fahrenheit Value: ', fahrenheitValue)
-print('Kelvin Value: ', kelvinValue)
+print('Fahrenheit Value: ', fahrenheit(celsiusVlaue))
+print('Kelvin Value: ', kelvin(celsiusVlaue))
